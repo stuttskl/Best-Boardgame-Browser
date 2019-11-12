d97d612fd63a659d9c2e0f9fd3964ad9e3957b01
Katie <25576005+stuttskl@users.noreply.github.com>
2019-11-11 21:57:53 -0800

    Update app.js
    update server address

--- app.js
+++ app.js
@@ -172,6 +172,6 @@ app.use(function (err, req, res, next) {
 });
 
 app.listen(app.get('port'), function() {
-    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+    console.log('flip1.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
 
