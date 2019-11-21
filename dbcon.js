f4edba55dc10bdaa892c4e9743b10db4015023dc
Katie <25576005+stuttskl@users.noreply.github.com>
2019-11-20 21:55:35 -0800

    updated dbcon.js to test locally

--- dbcon.js
+++ dbcon.js
@@ -1,33 +1,33 @@
-// let mysql = require('mysql');
-// let pool = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'password',
-//     database: 'boardgame_db'
-// });
+ let mysql = require('mysql');
+ let pool = mysql.createConnection({
+     host: 'localhost',
+     user: 'root',
+     password: 'password',
+     database: 'group27_test'
+ });
 
-// pool.connect(function(err) {
-//     if (err) {
-//       return console.error('error: ' + err.message);
-//     }
-//     console.log('Connected to the MySQL server.');
-// });
-
-// module.exports.pool = pool;
-
-let mysql = require('mysql');
-let pool = mysql.createConnection({
-    host: 'classmysql.engr.oregonstate.edu',
-    user: 'cs340_stuttsk',
-    password: '7172',
-    database: 'cs340_stuttsk'
-});
-
-pool.connect(function(err) {
-    if (err) {
-      return console.error('error: ' + err.message);
-    }
-    console.log('Connected to the MySQL server.');
-});
+ pool.connect(function(err) {
+     if (err) {
+       return console.error('error: ' + err.message);
+     }
+     console.log('Connected to the MySQL server.');
+ });
 
 module.exports.pool = pool;
+
+//let mysql = require('mysql');
+//let pool = mysql.createConnection({
+//    host: 'classmysql.engr.oregonstate.edu',
+//    user: 'cs340_stuttsk',
+//    password: '7172',
+//    database: 'cs340_stuttsk'
+//});
+
+//pool.connect(function(err) {
+//    if (err) {
+//      return console.error('error: ' + err.message);
+//    }
+//    console.log('Connected to the MySQL server.');
+//});
+
+//module.exports.pool = pool;
