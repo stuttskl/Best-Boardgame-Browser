b9067d66facf099105f37a6f69a33311652362b7
Zajano <50506522+Zajano@users.noreply.github.com>
2019-11-27 19:54:27 -0800

    Update category.js
    pasted in add game to category stuff

--- public/js/category.js
+++ public/js/category.js
@@ -1,17 +1,40 @@
 module.exports = function () {
   var express = require('express');
   var router = express.Router();
+  var async = require('async');
 
-  function getCategory(res, mysql, context, complete) {
-    mysql.pool.query("SELECT id, category FROM category", function (err, results) {
-      if (err) {
-        res.write(JSON.stringify(err));
-        res.end();
-      }
-      context.category = results;
-      complete();
-    });
-  };
+//   function getCategory(res, mysql, context, complete) {
+//     mysql.pool.query("SELECT id, category FROM category", function (err, results) {
+//       if (err) {
+//         res.write(JSON.stringify(err));
+//         res.end();
+//       }
+//       context.category = results;
+//       complete();
+//     });
+//   };
+  
+    function getAllCats(mysql) {
+    return function(callback) {
+      mysql.pool.query('SELECT id, category FROM category;', function(err, data1) {
+        if (err) {
+          return callback(err, []);
+        }
+        return callback(null, data1);
+      });
+    }
+  }
+
+  function getAllGames(mysql) {
+    return function(callback) {
+      mysql.pool.query('SELECT id, game_name, max_players, min_players FROM games;', function(err, data2) {
+        if (err) {
+          return callback(err, []);
+        }
+        return callback(null, data2);
+      });
+    }
+  }
 
   function searchFunction(req, res, mysql, context, complete) {
     //sanitize the input as well as include the % character
@@ -26,6 +49,21 @@ module.exports = function () {
       complete();
     });
   };
+  
+    router.post('/addGameCat', function (req, res) {
+    var mysql = req.app.get('mysql');
+    var sql = "INSERT INTO game_category (`category_id`, `game_id`) VALUES (?, ?)";
+    var inserts = [req.body.category_selection, req.body.game2_selection];
+    sql = mysql.pool.query(sql, inserts, function (err) {
+      if (err) {
+        console.log(JSON.stringify(err))
+        res.write(JSON.stringify(err));
+        res.end();
+      } else {
+        res.redirect('/category');
+      }
+    });
+  });
 
 router.post('/delete', function (req, res) {
    var mysql = req.app.get('mysql');
@@ -44,17 +82,21 @@ router.post('/delete', function (req, res) {
   });
 
   router.get('/', function (req, res) {
-    var callbackCount = 0;
-    var context = {};
-    var mysql = req.app.get('mysql');
-    getCategory(res, mysql, context, complete);
-    function complete() {
-      callbackCount++;
-      if (callbackCount >= 1) {
-        res.render('category', context);
-      };
-    };
-  });
+		var mysql = req.app.get('mysql');
+    async.parallel(
+      {
+        category: getAllCats(mysql),
+        games: getAllGames(mysql)
+      },
+      function(err, results) {
+        if (err) {
+          console.log(err.message);
+        }
+        res.render('category', results); // { players: [], games: [] }
+      }
+    );
+	});
+
 
   router.get('/search', function (req, res) {
     var callbackCount = 0;
