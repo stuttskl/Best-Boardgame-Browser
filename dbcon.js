20c857c160efd3518c7e0fbf3980a5fe280b7631
Katie <25576005+stuttskl@users.noreply.github.com>
2020-08-29 13:12:34 -0700

    Revert "fixed broken SQL queries"
    This reverts commit e157dd0f1fa41f7efd6947c2f753018090d4330b.

--- dbcon.js
+++ dbcon.js
@@ -1,18 +1,16 @@
-
 let mysql = require('mysql');
-
 let pool = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'bbb'
+  host: 'oniddb.cws.oregonstate.edu',
+  user: 'stuttsk-db',
+  password: 'xxxxxxx',
+  database: 'stuttsk-db'
 });
 
 pool.connect(function(err) {
-    if (err) {
-      return console.error('error: ' + err.message);
-    }
-    console.log('Connected to the MySQL server.');
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+  console.log('Connected to the MySQL server.');
 });
 
 module.exports.pool = pool;
